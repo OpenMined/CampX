9c80326d3ba4401aed1a340d61d2c47d1f169fa4
iamtrask <liamtrask@gmail.com>
2018-07-10 12:13:14 +0100

    re-init

--- par_q_learn.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..20}; do
-  python run_q_learning_agent.py &
-  sleep 2
-done
--- par_random.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {1..20}; do
-  python run_random_agent.py &
-  sleep 2
-done
