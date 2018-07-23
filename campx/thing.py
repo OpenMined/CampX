8bc75f479418bcc10ec3c8139f8cd7e0426b0f88
Jason Mancuso <jvmancuso87@gmail.com>
2018-07-23 19:49:22 -0400

    stashed unfinished refactor; changing strategy but wanted to keep some changes from this

--- campx/thing.py
+++ campx/thing.py
@@ -1,23 +1,39 @@
 import torch
+from abc import ABC
 
-class AbstractThing(object):
-    
+class AbstractThing(ABC):
     def __init__(self, init_x=None, init_y=None, identity=1):
-        self.init_x = init_x
-        self.init_y = init_y
+        self.x = init_x
+        self.y = init_y
         self.identity = identity
-        
-    def create_world(self, rows, cols):
-        w = torch.zeros(rows, cols)
-        w[self.init_x][self.init_y] += self.identity
-        self.w = w
-        return w
 
-class Thing(AbstractThing):
-    def __init__(self, x=None, y=None, hover_reward=1, category="apple"):
+class EnvThing(AbstractThing, ABC):
+    def __init__(self, x=None, y=None, hover_reward=1):
         super().__init__(init_x=x, init_y=y)
         self.hover_reward = hover_reward
-        self.category = category
-        
+    
+    @abstractmethod
+    def update(self):
+        pass
+
     def render_reward_mask(self):
-        return self.w * self.hover_reward
+        return self.world * self.hover_reward
+
+    def create_world(self, tensor_world):
+        world = torch.zeros(tensor_world.rows, tensor_world.cols)
+        world[self.init_x][self.init_y] = self.identity
+        self.world = world
+        return world
+
+class WrapAgent(AbstractThing):
+    """Wraps an agent for compatibility with safe-grid-agents
+    (see https://github.com/jvmancuso/safe-grid-agents)
+    """
+    def __init__(self, agent, init_x=None, init_y=None):
+        super(WrapAgent, self).__init__(init_x, init_y, 1) # assuming agent identity is 1
+
+    def __call__(self, agent):
+        agent.x = self.x
+        agent.y = self.y
+        agent.identity = self.identity
+        self = agent
--- campx/world.py
+++ campx/world.py
@@ -2,57 +2,75 @@ import torch
 
 class TensorWorld(object):
     
-    def __init__(self, rows, cols, things=None, agent=None):
+    def __init__(self, rows, cols, channels=1, things=None, agent=None):
         self.rows = rows
         self.cols = cols
         
-        if(things is None):
+        if things is None:
             self.things = list()
         else:
             self.things = things
+        if agent is not None:
+            self.agent = self.add_agent(agent)
+        self.thing_registry = {}
 
-        self.agent = agent
-        self.cat2things = {}
+        self.reset()
+
+    def step(self, action):
+        self.update(action)
+        for thing in self.things:
+            thing.update(action)
+        reward = self._get_reward()
+        # TODO
+        # done = self._get_done()
+        # info = self._gather_info() # needs to match
+        done, info = None, None
+        return reward, done, info, successor
     
-    def add_thing(self, thing):
+    def register_thing(self, thing):
         self.things.append(thing)
-        if(thing.category not in self.cat2things):
-            self.cat2things[thing.category] = set()
-        self.cat2things[thing.category].add(thing)
+        if thing.kind not in self.thing_registry:
+            self.thing_registry[thing.kind] = list()
+        self.thing_registry[thing.kind].append(thing)
         
-    def set_agent(self, agent):
+    def add_nonagent(self, thing):
+        self.register_thing(thing)
+        self.world[]
+
+    def add_agent(self, agent):
         self.agents = agent
         
     def reset(self):
+        self.world = self.create_world()
 
-        for thing in self.things:
-            thing.create_world(self.rows, self.cols)
-            
-        self.agent.create_world(self.rows, self.cols)
-        
+    def create_world(self):
+        return torch.zeros(self.rows, self.cols, self.channels)
+
     def render(self):
         dims = list()
-        for cat, things in self.cat2things.items():
-            dims.append(sum(map(lambda x:x.w,things)))
-        dims.append(self.agent.w)
+        for cat, things in self.thing_registry.items():
+            dims.append(sum(map(lambda x:x.world,things)))
+        dims.append(self.world)
         return torch.cat(dims)
 
-    def reward(self):
-        return (sum(map(lambda x:x.render_reward_mask(),self.things)) * self.agent.w).sum()
-        
-    def update(self, y):
-        left = torch.cat([self.agent.w[:,1:],self.agent.w[:,:1]], dim=1)
-        right = torch.cat([self.agent.w[:,-1:],self.agent.w[:,:-1]], dim=1)
-        up= torch.cat([self.agent.w[1:],self.agent.w[:1]], dim=0)
-        down = torch.cat([self.agent.w[-1:],self.agent.w[:-1]], dim=0)
-        stay = self.agent.w
+    def _get_reward(self):
+        return (sum(map(lambda x:x.render_reward_mask(), self.things)) * self.world).sum()
 
-        new_state = (y[0] * left) + (y[1] * right) + (y[2] * up) + (y[3] * down) + (y[4] * stay)
-        self.agent.w = new_state
+    def update(self, action):
+        left = torch.cat([self.agent.world[:,1:],self.agent.world[:,:1]], dim=1)
+        right = torch.cat([self.agent.world[:,-1:],self.agent.world[:,:-1]], dim=1)
+        up= torch.cat([self.agent.world[1:],self.agent.world[:1]], dim=0)
+        down = torch.cat([self.agent.world[-1:],self.agent.world[:-1]], dim=0)
+        stay = self.world
+
+        new_state = (action[0] * left) + (action[1] * right) + (action[2] * up) + (action[3] * down) + (action[4] * stay)
+        self.world = new_state
 
     def step(self, action):
-        action = torch.FloatTensor([0,1,0,0,0])
-        self.update(action)
-        reward = self.reward()
+        action_tensor = torch.zeroes(5)
+        action_tensor[action] = 1
+        self.update(action_tensor)
+        reward = self._get_reward()
         observation = self.render()
-        return observation, reward
+        return observation, reward, None
+
