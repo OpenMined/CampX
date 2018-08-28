eb0eda0e8191a7c8f31403765ab6b43ad166f1a3
iamtrask <liamtrask@gmail.com>
2018-08-28 15:03:54 +0100

    removed unused methods

--- campx/engine.py
+++ campx/engine.py
@@ -260,43 +260,6 @@ class Engine(object):
         # Done with all the layers; render the board!
         self._board = self._renderer.render()
 
-    def set_agent(self, agent):
-        self.agents = agent
-
-    def reset(self):
-
-        for thing in self.things:
-            thing.create_world(self.rows, self.cols)
-
-        self.agent.create_world(self.rows, self.cols)
-
-    def render(self):
-        dims = list()
-        for cat, things in self.cat2things.items():
-            dims.append(sum(map(lambda x: x.w, things)))
-        dims.append(self.agent.w)
-        return torch.cat(dims)
-
-    def reward(self):
-        return (sum(map(lambda x: x.render_reward_mask(), self.things)) * self.agent.w).sum()
-
-    def update(self, y):
-        left = torch.cat([self.agent.w[:, 1:], self.agent.w[:, :1]], dim=1)
-        right = torch.cat([self.agent.w[:, -1:], self.agent.w[:, :-1]], dim=1)
-        up = torch.cat([self.agent.w[1:], self.agent.w[:1]], dim=0)
-        down = torch.cat([self.agent.w[-1:], self.agent.w[:-1]], dim=0)
-        stay = self.agent.w
-
-        new_state = (y[0] * left) + (y[1] * right) + (y[2] * up) + (y[3] * down) + (y[4] * stay)
-        self.agent.w = new_state
-
-    def step(self, action):
-        action = torch.FloatTensor([0, 1, 0, 0, 0])
-        self.update(action)
-        reward = self.reward()
-        observation = self.render()
-        return observation, reward
-
     def _runtime_error_if_called_during_showtime(self, method_name):
         if self._showtime:
             raise RuntimeError('{} should not be called after its_showtime() '
