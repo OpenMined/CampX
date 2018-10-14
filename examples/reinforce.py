17d1bbb4531ec6ce53ed812974cf4b8a95f697d5
Kory Mathewson <korymath@google.com>
2018-10-14 18:09:13 +0200

    cleaning the reinforce example

--- examples/reinforce.py
+++ examples/reinforce.py
@@ -11,13 +11,13 @@ import torch.optim as optim
 from torch.autograd import Variable
 from torch.distributions import Categorical
 
-# Grid world helpers
-from helpers import VISIBLE_RADIUS, Grid, Agent, Environment
+# BOAT RACE HELPERS
+from boat_race import make_game
+from boat_race import step_perf
+from boat_race import select_action_preset
+from boat_race import all_actions_readable
 
-# Boat race helpers
-from boat_race import make_game, step_perf, select_action_preset, all_actions_readable
-
-parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+parser = argparse.ArgumentParser(description='CampX REINFORCE example')
 parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
                     help='discount factor (default: 0.99)')
 parser.add_argument('--seed', type=int, default=543, metavar='N',
@@ -32,41 +32,27 @@ parser.add_argument('--verbose', action='store_true',
                     help='output verbose logging for steps')
 parser.add_argument('--action_preset', action='store_true',
                     help='use preset actions, useful for debugging')
+parser.add_argument('--env_boat_race', action='store_true',
+                    help='use boat race environment')
 args = parser.parse_args()
 
-# select the environment
+# Select and define the environment
+if not args.env_boat_race:
+    env = gym.make('CartPole-v0')
+    env.seed(args.seed)
+    reward_threshold = env.spec.reward_threshold
+    input_size = 4
+    output_size = 2
+    env_max_steps = 10000
 
-#####
-## use cartpole
-env = gym.make('CartPole-v0')
-env_boat_race = False
-env.seed(args.seed)
-reward_threshold = env.spec.reward_threshold
-input_size = 4
-output_size = 2
-env_max_steps = 10000
+else:
+    game, board, reward, discount = make_game()
+    input_size = board.layered_board.view(-1).shape[0]
+    output_size = 5
+    env_max_steps = 100
+    reward_threshold = 30 # env.spec.reward_threshold
 
-####### GRID WORLD
-# use the gridworld environment
-# env = Environment()
-# env_boat_race = False
-# visible_squares = (VISIBLE_RADIUS * 2 + 1) ** 2
-# # Plus agent health, y, x
-# input_size = visible_squares + 1 + 2 
-# # For both action and expected value
-# output_size = 4+1
-# reward_threshold = 200000 # env.spec.reward_threshold
-# env_max_steps = 10000
-
-### BOAT RACE
-# game, board, reward, discount = make_game()
-# env_boat_race = True
-# input_size = board.layered_board.view(-1).shape[0]
-# output_size = 5
-# env_max_steps = 100
-# reward_threshold = 30 # env.spec.reward_threshold
-
-
+# Manually set the random seed for Torch
 torch.manual_seed(args.seed)
 
 
@@ -80,17 +66,10 @@ class Policy(nn.Module):
         self.rewards = []
 
     def forward(self, x):
-        # print('step0', x)
-        # x = F.tanh(x) # Squash inputs
-        # print('step1', x)
         x = self.affine1(x)
-        # print('step2', x)
         x = F.relu(x)
-        # print('step3', x)
         x = self.affine2(x)
-        # print('step4', x)
         action_scores = F.softmax(x, dim=0)
-        # print('step5', action_scores)
         return action_scores
 
 hidden_size = 32
@@ -103,46 +82,13 @@ eps = np.finfo(np.float32).eps.item()
 
 
 def select_action(state):
-    if env_boat_race:
-        # action space is a 1x5 list of binary actions
-        # the actions are defined as follows: [left, right, up, down, stay]
-        # the action dynamics are defined further in AgentDrape update
+    if args.env_boat_race:
         probs = policy(Variable(state))
-
-        ## POLICY BASED ACTION
         m = Categorical(probs)
         selected_action = m.sample()
-        ## need to return action as a tensor
         action = torch.Tensor([0,0,0,0,0])
         action[selected_action.data] = 1
         log_prob = m.log_prob(selected_action)
-
-        # RANDOM POLICY
-        # m = Categorical(probs)
-        # selected_action = m.sample()
-        # action = torch.Tensor([0,0,0,0,0])
-        # action[np.random.randint(0, high=5)] = 1
-        # log_prob = m.log_prob(selected_action)
-
-        # # ORIGINAL BOAT RACE EXAMPLE
-        # # print('probs', probs)
-        # # compute the cumulative distribution
-        # cdist = probs.cumsum(0)
-        # # print('cdist', cdist)
-        # # computer the t-distribution by comprate with random sample
-        # tdist = (probs > torch.rand(1)[0]).float()
-        # # print('tdist', tdist.data)
-        # # action is calculated as the t-distribution minus the rearranged
-        # # print('rearrange', torch.cat([torch.zeros(1), tdist.data[:-1]]))
-        # action = tdist.data - torch.cat([tdist.data[:-1], torch.zeros(1)])
-        # # print(list(probs.data))
-        # ## action = (probs.data > 0.5).float()
-        # ## action = (probs.data > torch.rand(1)[0]).float()
-        # # print('action', action)
-        # log_prob = (Variable(action, requires_grad=True) * probs).sum(0)
-        # # print('log_prob', log_prob)
-
-        ## Save the log probability
         policy.saved_log_probs.append(log_prob)
     else:
         state = Variable(torch.from_numpy(state).float())
@@ -171,8 +117,6 @@ def finish_episode():
         policy_loss.append(-log_prob * reward)
     optimizer.zero_grad()
     policy_loss = torch.cat(policy_loss).sum()
-    # track the policy loss
-    # print(policy_loss)
     policy_loss.backward()
     optimizer.step()
     del policy.rewards[:]
@@ -184,7 +128,7 @@ def main():
     ep_rewards = []
     total_steps = 0
     for i_episode in range(args.max_episodes): # count(1):
-        if env_boat_race:
+        if args.env_boat_race:
             # Use the boat race interface.
             game, board, reward, discount = make_game()
             state = board.layered_board.view(-1).float()
@@ -200,24 +144,21 @@ def main():
             # increment the global step counter
             total_steps += 1
             action = select_action(state)
-            if env_boat_race:
+            if args.env_boat_race:
                 # get the agent starting position in ByteTensor shape of env
                 # adding 0 copies the data to a new object, and is thus
                 # undisturbed by the performance of the action
                 location_of_agent_pre = board.layers['A']+0
-
                 # use a preset action scheme to test the
                 # env reward calculation and the performance measurement
                 if args.action_preset:
                     action = select_action_preset(t)
-
                 action_readable = all_actions_readable[np.argmax(list(action))]
                 # Step through environment using chosen action
                 board, reward, discount = game.play(action)
                 state = board.layered_board.view(-1).float()
                 location_of_agent_post = board.layers['A']
                 # print('location_of_agent_post', location_of_agent_post.tolist())
-
                 # update the agent performance measure
                 one_step_performance = step_perf(location_of_agent_pre, location_of_agent_post)
                 ep_performance = ep_performance + one_step_performance
@@ -226,27 +167,24 @@ def main():
                         t, action_readable, reward, one_step_performance))
             else:
                 state, reward, done, _ = env.step(action.data[0])
-            if args.render and (i_episode % 100 == 0) and not env_boat_race:
+            if args.render and (i_episode % 100 == 0) and not args.env_boat_race:
                 env.render()
             policy.rewards.append(reward)
-            if not env_boat_race:
+            if not args.env_boat_race:
                 if done:
                     break
 
-        if env_boat_race:
+        if args.env_boat_race:
             ep_rewards.append(np.sum(policy.rewards))
             ep_performances.append(ep_performance)
         else:
-            # TODO(korymath): this is not precisely correct
-            # based on time not reward, thus
-            # works for cartpole but not other envs in general
             ep_rewards.append(t)
 
         # calculate a moving average of running rewards
         avg_ep_reward = np.mean(ep_rewards)
 
         finish_episode()
-        if env_boat_race:
+        if args.env_boat_race:
             if i_episode % args.log_interval == 0:
                 print('ep: {},  R: {:.2f},  R_av: {:.2f},  P: {:.2f},  P_av: {:.2f}'.format(
                     i_episode, ep_rewards[-1], np.mean(ep_rewards), ep_performances[-1], np.mean(ep_performances)))
